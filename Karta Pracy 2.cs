ba720dbca3fb0bb2af81a3de6b35e620bc4afd6a
Reshenee <115945551+Reshenee@users.noreply.github.com>
2022-11-27 23:48:39 +0100

    Add files via upload

---
+++ Karta Pracy 2.cs
@@ -0,0 +1,77 @@
+﻿//Zadanie 1
+
+/*int n = int.Parse(Console.ReadLine());
+if (n % 3 == 0)
+{
+    Console.WriteLine("Jest podzielna");
+}
+else
+{
+    Console.WriteLine("Nie jest podzielna");
+}*/
+
+
+
+//Zadanie 2
+
+/*int n = int.Parse(Console.ReadLine());
+if (n >= 100 && n <= 999 && n % 17 == 0)
+{
+    Console.WriteLine("Liczba jest 3-cyfrowa i podzielna przez 17");
+}
+else
+{
+    Console.WriteLine("Liczba nie spełnia warunków");
+}
+*/
+
+
+
+//Zadanie 3
+
+
+/*Console.WriteLine("Wpisz swój wiek.");
+int n = int.Parse(Console.ReadLine());
+if (n >= 18)
+{
+    Console.WriteLine("Jesteś Pełnoletni");
+}
+else
+{
+    Console.WriteLine("Nie Jesteś Pełnoletni");
+}*/
+
+
+
+//Zadanie 3 Wersja 2
+
+
+
+/*int wiek = int.Parse(Console.ReadLine());
+Console.WriteLine(wiek >= 18 ? "Jesteś Pełnoletni" : "Nie Jesteś Pełnoletni");
+*/
+
+
+
+//Zadanie 4
+
+/*Console.WriteLine("Ile Waży Twoja Ciężarówka");
+int waga = int.Parse(Console.ReadLine());
+if (waga <= 20)
+{
+    Console.WriteLine("Możesz Wjechać Na Most")
+    ;
+}
+else
+{
+    Console.WriteLine("Nie Możesz Wjechać Na Most");
+}*/
+
+
+//Zadanie 4 Wersja 2
+
+
+/*int waga = int.Parse(Console.ReadLine());
+Console.WriteLine(waga <= 20 ? "Możesz Wjechać Na Most" : "Nie Możesz Wjechać Na Most");*/
+
+
