281b577bcf80e78a1d7cac298bfd794946dd9552
Reshenee <115945551+Reshenee@users.noreply.github.com>
2022-11-27 23:47:55 +0100

    Add files via upload

---
+++ Zadanie Domowe RD1E.cs
@@ -0,0 +1,115 @@
+// Zadanie 1
+// int a = int.Parse(Console.WriteLine());
+// int b = int.Parse(Console.WriteLine());
+// if ((a + b) % 2 == 0)
+// {
+//     Console.WriteLine("TAK");
+// }
+// else
+// {
+//     Console.WriteLine("NIE");
+// }
+
+// Zadanie 2
+
+// int a = int.Parse(Console.ReadLine());
+// int g = int.Parse(Console.ReadLine());
+// if (((a + g) / 2) > (Math.Pow((a * g) * 0.5)))
+// {
+//     Console.WriteLine("TAK");
+// }
+// else
+// {
+//     Console.WriteLine("NIE");
+// }
+
+//  Zadanie 3
+
+// int k = int.Parse(Console.ReadLine());
+// int l = int.Parse(Console.ReadLine());
+// int m = int.Parse(Console.ReadLine());
+
+// if ((k == l) && !(k == m) && !(l == m))
+// {
+//     Console.Write("k = l = ");
+//     Console.WriteLine(k);
+// }
+// if ((k == m) && !(k == l) && !(l == m))
+// {
+//     Console.Write("k = m = ");
+//     Console.WriteLine(m);
+// }
+// if ((l == m) && !(k == m) && !(l == k))
+// {
+//     Console.Write("l = m = ");
+//     Console.WriteLine(l);
+// }
+
+// Zadanie 4
+
+// int a = int.Parse(Console.ReadLine());
+// int b = int.Parse(Console.ReadLine());
+// int c = int.Parse(Console.ReadLine());
+// int d = int.Parse(Console.ReadLine());
+// if ((a < b) && (a < c) && (a < d))
+// {
+//     Console.WriteLine(a);
+// }
+// if ((b < a) && (b < c) && (b < d))
+// {
+//     Console.WriteLine(b);
+// }
+// if ((c < a) && (c < b) && (c < d))
+// {
+//     Console.WriteLine(c);
+// }
+// if ((d < a) && (d < b) && (d < c))
+// {
+//     Console.WriteLine(d);
+// }
+
+
+// Zadanie 5
+
+// int a = int.Parse(Console.ReadLine());
+// int b = int.Parse(Console.ReadLine());
+// int c = int.Parse(Console.ReadLine());
+// if ((a < (b+c)) && (b < (a + c)) && (c < (a + b)))
+// {
+//     Console.WriteLine("TAK");
+// }
+// else
+// {
+//     Console.WriteLine("NIE");
+// }    
+
+
+// Zadanie 6
+
+// int a = int.Parse(Console.ReadLine());
+// int b = int.Parse(Console.ReadLine());  
+// int c = int.Parse(Console.ReadLine());
+
+
+// if ((a * a + b * b == c * c) && (a * a + b * b == c * c) && (c * c + b * b == a * a))
+// {
+//     Console.WriteLine("Prostokątny");
+// }
+// else
+// {
+//     if ((a * a + b * b < c * c) && (a * a + b * b < c * c) && (c * c + b * b < a * a))
+//     {
+//         Console.WriteLine("Rozwartokątny");
+//     }
+//     else
+//     {
+//         if ((a * a + b * b > c * c) && (a * a + b * b > c * c) && (c * c + b * b > a * a))
+//         {
+//             Console.WriteLine("Ostrokątny");
+//         }
+//         else
+//         {
+
+//         }
+//     }
+// }
