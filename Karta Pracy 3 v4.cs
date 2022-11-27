39ea5bd3e820757a1d76a14eb273b68aa206c8db
Reshenee <115945551+Reshenee@users.noreply.github.com>
2022-11-27 23:48:53 +0100

    Add files via upload

---
+++ Karta Pracy 3 v4.cs
@@ -0,0 +1,57 @@
+﻿/*Zadanie 3*/
+
+
+/*int n = int.Parse(Console.ReadLine());
+for (int i = 1; i < n + 1; i++)
+{
+    Console.Write(n % i == 0 ? i + " " : "");
+}
+
+Console.ReadKey();*/
+
+
+/*Zadanie 4*/
+
+/*int suma = 0;
+for (int i = 10; i < 100; i++)
+{
+    suma = (suma + i);
+    Console.WriteLine(suma);
+}
+Console.ReadKey();*/
+
+/*Zadanie 5*/
+
+
+/*int n = int.Parse(Console.ReadLine());
+
+int suma = n * (n + 1) / 2;
+int liczba;
+
+for (int i = 0; i < n - 1; i++)
+{
+    liczba = int.Parse(Console.ReadLine());
+    suma = suma - liczba;
+}
+Console.WriteLine("");
+Console.WriteLine(suma);
+Console.ReadKey();*/
+
+
+/*Zadanie 6*/
+
+/*int n = int.Parse(Console.ReadLine());
+int a = 0;
+int b = 1;
+int temp;
+for (int i = 0; i < n; i++)
+{
+    temp = a;
+    a = b;
+    b = temp + b;
+}
+{
+    Console.WriteLine(b);
+}
+
+Console.ReadKey();*/
