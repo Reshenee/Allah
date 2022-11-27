f2cf2bcddd845e7a288795f35ae99e0bd7438e42
Reshenee <115945551+Reshenee@users.noreply.github.com>
2022-11-27 23:50:00 +0100

    Add files via upload

---
+++ Petle.py
@@ -0,0 +1,21 @@
+# for i in range(10,22): print (i, end=" ")
+
+# for i in range (15,32,2): print (i, end=" ")
+
+# for i in range (99,9,-1): print (i, end=" ")
+
+# for i in range(10,100,1): print(109-i, end=" ")
+
+# for i in range (999,99,-20): print (i, end=" ")
+
+# suma = 0
+# for i in range (10,100):
+#     suma = suma + i
+# print (suma)
+
+# n = int(input())
+# suma = n * (n + 1) / 2
+# for i in range (n-1):
+#     x = int(input())
+#     suma = suma - x
+# print(suma)
