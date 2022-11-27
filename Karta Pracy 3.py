f141255c0af31ca53f341ece3264fca8c4061ae3
Reshenee <115945551+Reshenee@users.noreply.github.com>
2022-11-27 23:49:40 +0100

    Add files via upload

---
+++ Karta Pracy 3.py
@@ -0,0 +1,36 @@
+# for i in range(10,22): print (i, end=" ")
+
+# for i in range (15,32,2): print (i, end=" ")
+
+# for i in range (99,9,-1): print (i, end=" ")
+
+# for i in range(10,100,1): print(109-i, end=" ")
+
+# for i in range (999,99,-20): print (i, end=" ")
+
+# suma = 0
+# for i in range (10,100):
+#     suma = suma + i
+# print (suma)
+
+# n = int(input())
+# suma = n * (n + 1) / 2
+# for i in range (n-1):
+#     x = int(input())
+#     suma = suma - x
+# print(suma)
+
+# n = int(input())
+# a = 1
+# b = 2
+# for i in range (n-2):
+#     a, b = b, a+b
+#     print(b, end=" ")
+
+
+
+# n = int(input())
+# silnia = 1
+# for i in range(1,n+1):
+#     silnia = silna * i
+# print(silnia)
