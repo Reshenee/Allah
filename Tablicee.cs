35edb1c914febf9f169d619170315dda13c06a33
Reshenee <115945551+Reshenee@users.noreply.github.com>
2022-11-30 14:21:55 +0100

    Add files via upload

---
+++ Tablicee.cs
@@ -0,0 +1,41 @@
+﻿/*int a = 3;
+int b = 5;
+int c = 6;
+int d = 7;
+
+int[] T = new int[4];
+T[0] = 3;
+T[1] = 5;
+T[2] = 6;
+T[3] = 7;*/
+
+/*int[] L = new int[] { 3, 5, 6, 7 };*/
+
+
+/*for (int i = 0; i < L.Length; i++)
+{
+    Console.WriteLine (L[i]);
+}*/
+
+
+
+
+
+
+
+
+
+
+
+/*int n = int.Parse(Console.ReadLine());
+int[] W = new int[n];*/
+
+
+
+int[] T = new int[n];
+suma == 0 
+for (int i = 0; i < T.Length; i++)
+{
+    suma += T[i]
+}
+Console.WriteLine(suma)
