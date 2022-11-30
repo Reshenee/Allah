c4e9aab9180dd1b15eb74aececaef4a6d65c619b
Reshenee <115945551+Reshenee@users.noreply.github.com>
2022-11-30 14:21:28 +0100

    Add files via upload

---
+++ Algorytmy.py
@@ -0,0 +1,14 @@
+# a = int(input())
+# b = int(input())
+#
+#
+# while a != b:
+#     if a > b : a = a - b
+#     elif b > a : b = b - a
+# print (a)
+
+
+# a, b = int(input()), int(input())
+# while b > 0:
+#     a, b = b, a % b
+# print (a)
