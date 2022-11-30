70fca20e8abf38861dd1212b89611e337ee3691a
Reshenee <115945551+Reshenee@users.noreply.github.com>
2022-11-30 14:20:48 +0100

    Add files via upload

---
+++ Tablicegood.cs
@@ -0,0 +1,53 @@
+﻿/*int n = int.Parse(Console.ReadLine());
+int[] W = new int[n];
+for (int i = 0; i < W.Length; i++)
+{
+    W[i] = int.Parse(Console.ReadLine());
+}
+for (int i = 0; i < W.Length; i++)
+{
+    Console.WriteLine(int.MaxValue);
+}*/
+
+/*int Maksik = int.MinValue;
+int n = int.Parse(Console.ReadLine());
+int[] W = new int[n];
+int ilosc = 1;
+for (int i = 0; i < W.Length; i++)
+{
+    W[i] = int.Parse(Console.ReadLine());
+}
+for (int i = 0; i < W.Length; i++)
+{
+    if (W[i] > Maksik)
+    {
+        Maksik = W[i];
+        ilosc = 0;
+    }
+}
+
+for (int i = 0; i < W.Length; i++)
+{
+    if (W[i] == Maksik);
+    {
+        ilosc += 1;
+    }
+}
+Console.WriteLine(ilosc);*/
+
+
+int Suma = 0;
+int n = int.Parse(Console.ReadLine());
+int[] W = new int[n];
+for (int i = 0; i < W.Length; i++)
+{
+    W[i] = int.Parse(Console.ReadLine());
+}
+for (int i = 0; i < W.Length; i++)
+{
+    if (W[i] % 2 == 0)
+    {
+        Suma += (W[i]);
+    }
+}
+Console.WriteLine(Suma);
