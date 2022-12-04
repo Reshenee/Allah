8796fd9a2f03a50f26dcf3ded485e31a42b452f9
Reshenee <115945551+Reshenee@users.noreply.github.com>
2022-12-05 00:37:20 +0100

    Add files via upload

---
+++ NWW z NWD.py
@@ -0,0 +1,12 @@
+# a = int(input())
+# b = int(input())
+#
+# iloczyn = a * b
+#
+# while a != b:
+#     if a > b:
+#         a = a - b
+#     elif a < b:
+#         b = b - a
+# NWD = a
+# print(iloczyn//NWD)
